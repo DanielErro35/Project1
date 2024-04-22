245a8f36fefe5d0b0b47cee597e7cbf1cb04def9
mys029 <dylanli777@gmail.com>
2024-04-22 11:59:27 -0700

    Made more changes

--- app.R
+++ app.R
@@ -29,24 +29,32 @@ server <- function(input, output, session) {
     relspeed <- input$relspeed
     spinrate <- input$spinrate
     
+    if (input$game == "Game 1") {
+      baseball_data <- baseball_22
+    }else if (input$game == "Game 2") {
+      baseball_data <- baseball_23
+    }else if (input$game == "Game 3") {
+      baseball_data <- baseball_24
+    }
+    
     # Apply filters
-    b <- baseball_22 %>%
+    b <- baseball_data %>%
       filter(
         RelSpeed >= relspeed,
         SpinRate >= spinrate
       ) %>%
       arrange(RelSpeed)
     
-    # # Optional: filter by genre
-    # if (input$genre != "All") {
-    #   genre <- paste0("%", input$genre, "%")
-    #   m <- m %>% filter(Genre %like% genre)
-    # }
-    # # Optional: filter by director
-    # if (!is.null(input$director) && input$director != "") {
-    #   director <- paste0("%", input$director, "%")
-    #   m <- m %>% filter(Director %like% director)
-    # }
+    # Optional: filter by Pitch Type
+    if (input$pitchtype != "All") {
+      pitchtype <- input$pitchtype
+      b <- b %>% filter(grepl(pitchtype, TaggedPitchType))
+    }
+    # # Optional: filter by pitcher
+    if (!is.null(input$pitcher) && input$pitcher != "") {
+      pitcher = input$pitcher
+      b <- b %>% filter(grepl(tolower(pitcher), tolower(Pitcher)))
+    }
     # # Optional: filter by cast member
     # if (!is.null(input$cast) && input$cast != "") {
     #   cast <- paste0("%", input$cast, "%")
@@ -58,26 +66,25 @@ server <- function(input, output, session) {
     
     # Add column which says whether the movie won any Oscars
     # Be a little careful in case we have a zero-row data frame
-    # m$has_oscar <- character(nrow(m))
-    # m$has_oscar[m$Oscars == 0] <- "No"
-    # m$has_oscar[m$Oscars >= 1] <- "Yes"
-    # m
+    b$team <- character(nrow(b))
+    b$team[b$PitcherTeam == "CAL_MUS"] <- "Cal Poly"
+    b$team[b$PitcherTeam == "SAN_GAU"] <- "UCSB"
+    b
   })
   
   # Function for generating tooltip text
-  # movie_tooltip <- function(x) {
-  #   if (is.null(x)) return(NULL)
-  #   if (is.null(x$ID)) return(NULL)
-  #   
-  #   # Pick out the movie with this ID
-  #   all_movies <- isolate(movies())
-  #   movie <- all_movies[all_movies$ID == x$ID, ]
-  #   
-  #   paste0("<b>", movie$Title, "</b><br>",
-  #          movie$Year, "<br>",
-  #          "$", format(movie$BoxOffice, big.mark = ",", scientific = FALSE)
-  #   )
-  # }
+  baseball_tooltip <- function(x) {
+    if (is.null(x)) return(NULL)
+    if (is.null(x$PitchNo)) return(NULL)
+
+    # Pick out the movie with this ID
+    baseball_22 <- isolate(baseball())
+    bb <- baseball_22[baseball_22$PitchNo == x$PitchNo, ]
+
+    paste0("<b>", bb$Pitcher, "</b><br>",
+           bb$TaggedPitchType, "<br>"
+    )
+  }
   
   # A reactive expression with the ggvis plot
   vis <- reactive({
@@ -93,9 +100,14 @@ server <- function(input, output, session) {
     baseball %>%
       ggvis(x = xvar, y = yvar) %>%
       layer_points(size := 50, size.hover := 200,
-                   fillOpacity := 0.2, fillOpacity.hover := 0.5) %>%
+                   fillOpacity := 0.2, fillOpacity.hover := 0.5,
+                   stroke = ~team, key := ~PitchNo) %>%
+      add_tooltip(baseball_tooltip, "hover") %>%
       add_axis("x", title = xvar_name) %>%
       add_axis("y", title = yvar_name) %>%
+      add_legend("stroke", title = "Pitcher's Team", values = c("Cal Poly Mustangs", "UCSB Gauchos")) %>%
+      scale_nominal("stroke", domain = c("Cal Poly", "UCSB"),
+                    range = c("forestgreen", "blue")) %>%
       set_options(width = 500, height = 500)
   })
   
@@ -112,9 +124,17 @@ ui <- fluidPage(
     column(3,
            wellPanel(
              h4("Filter"),
+             selectInput("game", "Game",
+                         c("Game 1", "Game 2", "Game 3")
+             ),
              sliderInput("relspeed", "Minimum speed of ball",
                          70, 90, 80, step = 2),
              sliderInput("spinrate", "Minimum spin rate of ball", 1100, 2600, 1800, step = 100),
+             selectInput("pitchtype", "Pitch Type",
+                         c("All", "ChangeUp", "Curveball", "Cutter", "Fastball", "FourSeamFastBall",
+                           "Slider", "TwoSeamFastBall")
+             ),
+             textInput("pitcher", "Pitcher name contains (e.g., 'matt', 'ager, matt')")
            ),
            wellPanel(
              selectInput("xvar", "X-axis variable", axis_vars, selected = "RelSpeed"),
